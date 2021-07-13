ba8ebce78d914e514c5e401bd8b98754b73f65eb
xiaoxindada <2245062854@qq.com>
2021-07-13 08:36:27 +0800

    guacamole: Enable permissive
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 skip_override androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_PREBUILT_KERNEL := device/oneplus/guacamole/prebuilt/Image.gz
